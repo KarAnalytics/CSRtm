f11d5bfc7b6e404616a66f0c08ad246784515b4d
Karthik Srinivasan <kartucson@gmail.com>
2020-01-30 11:16:03 -0700

    FOG index code updated
    Length of document and FOG index computed for sample dataset with 30 CSR file texts.
    Outcome is an R object with FOG index column included.
    
    Co-Authored-By: Jinhang Jiang <jinhangjiang@users.noreply.github.com>

---
+++ scripts/FOG_index.R
@@ -0,0 +1,34 @@
+## 1. Compute length of document (in terms of number of words)
+## 2. Compute the Fog-index based on (Loughran and McDonald, 2014) 
+
+library(stringr)
+library(koRpus)
+
+install.koRpus.lang("en")
+# load the package
+library(koRpus.lang.en)
+
+load("workspaces/CSR_documents_30samples.RData")
+
+dim(text_stack_sample)
+
+## Length of document in terms of number of words
+text_stack_sample$length = str_count(text_stack_sample[,1], '\\w+')
+
+## Readability or Fog-index of each document
+system.time(
+  for(row in 1:nrow(text_stack_sample))
+  {
+    print(row)
+    # temp = tokenize(text_stack[row,1], format = "obj", lang="en",doc_id=paste("sample_",row,sep="") )
+    if (text_stack_sample[row,1] != "")
+    {
+      temp = tokenize(text_stack_sample[row,1], format = "obj", lang="en",doc_id="sample" )
+      text_stack_sample[row,"FOG"] =   try(as.numeric(FOG(temp)[1])) 
+    }
+  }
+)
+
+text_stack_sample_FOG = text_stack_sample
+
+save(text_stack_sample_FOG, file = "workspaces/CSR_documents_30samples_FOG.RData")

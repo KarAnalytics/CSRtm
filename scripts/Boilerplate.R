05b5f65ec4164d22f13ce19019c84bc860fadb05
Jinhang Jiang <60441304+jinhangjiang@users.noreply.github.com>
2020-02-14 15:33:54 -0600

    Update Boilerplate.R

--- scripts/Boilerplate.R
+++ scripts/Boilerplate.R
@@ -4,7 +4,7 @@
 
 #### LOGIC:
 # Collect all tetragrams (ordered group of four words within each sentence) in each document in a list
-# Remove frequently used tetragrams: Remove tetragrams that occur 60% or more across documents 
+# Remove frequently used tetragrams: Remove tetragrams that occur 75% or more across documents 
 # from the list.   
 # Identify tetragrams that occur in at least 30% of the documents or an average of at least 5 times 
 # per document in the list (phrases commonly used in financial disclosures). Discard other tetragrams from the list.  
@@ -29,13 +29,13 @@ t2 <- unlist(tokenize_sentences(text_stack_sample[1,1]))
 summary(t2)
 
 ##############tokenize_sentences for all documents
-t<-rep(list(0),30)
+t<-list(length=30)
 for(row in 1:nrow(text_stack_sample))
 {
   print(row)
   if (text_stack_sample[row,1] != "")
   {
-    t[row]=unlist(tokenize_sentences(text_stack_sample[row,1]))
+    t[[row]] =unlist(tokenize_sentences(text_stack_sample[row,1]))
   }
 }
 
@@ -57,11 +57,16 @@ ngram <- list(length=length(t))
 for(i in 1:length(t))
 {
   print(i)
-  if(t[i] != "")
+  ngram[[i]] = list(length = length(t[[i]]))
+  for(j in 1:length(t[[i]]))
   {
-    ngram[i] = tokenize_ngrams(t[[i]],n=4)
+    if(t[[i]][[j]] != "")
+    {
+      ngram[[i]][[j]] = tokenize_ngrams(t[[i]][[j]],n=4)
+    }
   }
 }
+
 ngram[[4]][[1]]
 ####################labeling sentence number
 text_stack_sample$NumSenc= lapply(t,length) ###error
@@ -79,7 +84,7 @@ as.data.frame(table(text_stack_sample$ngrams))
 ## labeling their sentence number in a separate column or as a key value pair.
 ### For each tetragram, find the frequency (i.e., how many times, each tetragram used in each document).
 ### Append all tetragrams across all documents. Find the frequency of tetragrams across all documents. 
-### 30% and 60% cut-off can be set and the respective tetragrams can be identified. Using the sentence number, 
+### 30% and 75% cut-off can be set and the respective tetragrams can be identified. Using the sentence number, 
 ### we can find out the number of words in that sentence to find the number of words in that sentence. 
 ### Finally this can be used to find the boilerplate score for each document. 
 

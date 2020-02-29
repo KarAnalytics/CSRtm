8bc0c683a54a3bd95c5c845a48b7ac72cac64272
Jinhang Jiang <60441304+jinhangjiang@users.noreply.github.com>
2020-02-29 16:50:32 -0600

    Update Redundancy.R

--- scripts/Redundancy.R
+++ scripts/Redundancy.R
@@ -1,22 +1,79 @@
-##### For document level:
+##################### Redundancy
+####### % of 10-grams that occur more than once in each document 
+load("workspaces/CSR_documents_30samples.RData")
 
-###############
+################## load all the packages
+library(koRpus.lang.en)
 
-for (i in 1:length(t))
+library(stringr)
+library(tokenizers)
+
+library(tidyverse)
+library(tm)
+library(rlist)
+
+#################  tokenize_sentences for all documents, we removed the numbers here as well
+t<-list(length=nrow(text_stack_sample))
+for(row in 1:nrow(text_stack_sample))
+{
+  print(row)
+  if (text_stack_sample[row,1] != "")
+  {
+    t[[row]] =unlist(tokenize_sentences(removeNumbers(text_stack_sample[row,1])))
+  }
+}
+
+################  10-ngrams for all documents
+ngram<- NULL
+for(i in 1:length(t))
 {
   print(i)
-  temp = 0
-  for (j in 1:nrow(N_table2))
+  ngram[[i]] = list(length = length(t[[i]]))
+  for(j in 1:length(t[[i]]))
   {
-    ngrams = na.omit(unlist(ngram[[i]]))
-    
-    if(any(str_detect(ngrams,as.character(N_table2[j,"Fngram"]))))
+    if(t[[i]][[j]] != "")
     {
-      temp = temp + 1 
+      ngram[[i]][[j]] = tokenize_ngrams(t[[i]][[j]],n=10)
     }
   }
-  text_stack_sample$DocFlag[i] = temp   
 }
+
+
+################ Calculate Redundancy
+#<- as.data.frame(table(unlist(ngram)))%>%arrange(desc(Freq))
+
+TenGram <- lapply(ngram,unlist)
+TenGram <- lapply(TenGram, table)
+TenGram <- lapply(TenGram, as.data.frame)
+
+for (i in 1:length(TenGram)){
+  text_stack_sample$Redundancy[i]<-sum(TenGram[i][[1]]$Freq>=2)/sum(TenGram[i][[1]]$Freq)
+}
+
+
+
+save(text_stack_sample, file = "workspaces/Redundancy.RData")
+
+
+
+##### For document level:
+###############
+
+#for (i in 1:length(t))
+#{
+#  print(i)
+#  temp = 0
+#  for (j in 1:nrow(N_table))
+#  {
+#    ngrams = na.omit(unlist(ngram[[i]]))
+#    
+#    if(any(str_detect(ngrams,as.character(N_table[j,"Fngram"]))))
+#    {
+#      temp = temp + 1 
+#    }
+#  }
+#  text_stack_sample$DocFlag[i] = temp   
+#}
 
 
 

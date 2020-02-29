7a5721a7c880dffc77bc900c0c3dcf67a5704a95
Jinhang Jiang <60441304+jinhangjiang@users.noreply.github.com>
2020-02-29 17:03:01 -0600

    Create Specificity.R

---
+++ scripts/Specificity.R
@@ -0,0 +1,7 @@
+#################################### 3.	Specificity:
+  
+#Extract named entities using Stanford NER tool. 
+#Specificity is the no. of specific entity names, quantitative values, times/dates 
+#all scaled by the total number of words in document.
+
+

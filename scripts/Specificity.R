de98c80a8597caa48a6ce971b2ffe5fbdb7e8518
Jinhang Jiang <60441304+jinhangjiang@users.noreply.github.com>
2020-03-02 09:50:43 -0600

    Update Specificity.R

--- scripts/Specificity.R
+++ scripts/Specificity.R
@@ -4,4 +4,22 @@
 #Specificity is the no. of specific entity names, quantitative values, times/dates 
 #all scaled by the total number of words in document.
 
+############# load packages
+library(koRpus.lang.en)
 
+library(stringr)
+library(tokenizers)
+
+library(tidyverse)
+library(tm)
+
+library(rJava)
+library(XML)
+
+library(coreNLP)
+
+downloadCoreNLP(type = "english")
+
+initCoreNLP()
+
+##################################
